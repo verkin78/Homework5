7c13079ba0f4dee692c7e517d20698ef0c1d3b7f
Verkin78 <otrajenie.jc@gmail.com>
2022-11-04 17:56:32 +0300

    Домашка5.3

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,12 @@ public class Main {
             System.out.println(" Установите облегчённую версию приложения для Android по ссылке ");        }
         else {System.out.println(" Ошибка ");}
 
+        //task5.3//
+        int year = 2021;
+        if (year %4 == 0 && year %100 != 0 || year %400 == 0) {
+            System.out.println(year + " год является високосным ");}
+        else {System.out.println(year + " год не является високосным ");}
+
+
     }
 }
