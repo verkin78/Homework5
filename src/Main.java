a955852d0688f9c01cf597ba0c2db88eeb40d28c
Verkin78 <otrajenie.jc@gmail.com>
2022-11-04 17:41:33 +0300

    Домашка5.2

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,20 @@ public class Main {
         else if (clientOS == 1) {
             System.out.println(" Установите версию приложения для Android по ссылке ");}
         else {System.out.println(" Ошибка ");}
+
+        //task5.2//
+        int client = 0;
+        int clientDeviceYear = 2015;
+
+        if (client == 0 && clientDeviceYear >= 2015) {
+            System.out.println(" Установите версию приложения для IOS по ссылке ");}
+        else if (client == 0 && clientDeviceYear < 2015) {
+            System.out.println(" Установите облегчённую версию приложения для IOS по ссылке ");}
+        if (client == 1 && clientDeviceYear >= 2015) {
+            System.out.println(" Установите версию приложения для Android по ссылке ");}
+        else if (client == 1 && clientDeviceYear < 2015) {
+            System.out.println(" Установите облегчённую версию приложения для Android по ссылке ");        }
+        else {System.out.println(" Ошибка ");}
+
     }
 }
