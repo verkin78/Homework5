8416dec1ceec459b1409100e830fdf21454afa34
Verkin78 <otrajenie.jc@gmail.com>
2022-11-04 17:15:56 +0300

    Домашка5.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task5.1//
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println(" Установите версию приложения для IOS по ссылке ");}
+        else if (clientOS == 1) {
+            System.out.println(" Установите версию приложения для Android по ссылке ");}
+        else {System.out.println(" Ошибка ");}
     }
 }
